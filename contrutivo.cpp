a30d050b2eff570e76a9a8282d873d3f5c01aa9c
Tassany <tassanyonofre@gmail.com>
2022-05-11 09:04:54 -0300

    Tentativa de interpretacao do algoritmo

--- contrutivo.cpp
+++ contrutivo.cpp
@@ -4,7 +4,7 @@
 #include<bits/stdc++.h> 
 using namespace std;
 
-#define ROW 4
+#define ROW 3
 #define COL 4
 
 
@@ -23,47 +23,39 @@ void printSolucao(vector<int> &sol, int arraySize)
 
 vector<int> construtivo(){
     
-    vector<vector<int>> pos=  {{0,40,20,100},{40,0,50,10},{20,50,0,10},{100,10,10,0}};
-    vector<int> solucao;
-    int sol = 0;
-    int menor = 1000;
-    int eita;
+    vector<vector<int>> costs = {
+        {0,40,20,100},
+        {40,0,50,10},
+        {20,50,0,10},
+        {100,10,10,0}
+    };
+    vector<int> vertices = { 2, 3, 4};
+    vector<int> sol = {1};
+    int solCost = 0;
+
+    int instanceSize = costs[1].size() - 1 ;
     int i,j;
-    // for(i=0; i< ROW;i++){
 
-    //     for(j=0; j<COL; j++){
-
-    //         if(pos[i][j] < menor && pos[i][j] != 0){
-    //             menor = pos[i][j];  
-    //         }    
-    //     }
-    //     solucao.push_back(menor);
-    // }
-
-    for(i=0; i < 1;i++){
-
-        for(j=0; j<COL; j++){
-
-            if(pos[i][j] < menor && pos[i][j] != 0){
-                menor = pos[i][j];
-                
-            }    
-        }
-        eita = j;
-        solucao.push_back(menor);
-    }
-
-
-    for(j=0; j<COL; j++){
-
-        if(pos[eita-1][j] < menor && pos[i][j] != 0){
-            menor = pos[i][j];  
-        }    
-    }
-    solucao.push_back(menor);
+    int menor = 1000;
     
-
-    return solucao;
+    while (!vertices.empty())
+    {
+        for(i = 0 ; i < ROW ; i++){
+            for(j = 0 ; j < vertices.size(); j++){
+                if(costs[sol[sol.size() - 1]][j] < menor && costs[sol[sol.size() - 1]][j] != 0){
+                    menor = costs[i][j];
+                }  
+            }
+            
+            int it = j - 1;
+            int tam = vertices.size();
+            sol.push_back(j);
+            solCost = solCost + menor;
+            vertices.erase(vertices.begin() +  it);
+        }
+    }
+    cout << solCost;
+    return sol;
 }
 
 int main()
