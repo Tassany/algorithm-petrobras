fb945c653629253adcceb096d5edafbed0ee1f19
Tassany <tassanyonofre@gmail.com>
2022-05-11 11:11:18 -0300

    Codigo construtivo com solucao correta

--- vizinhoProximo.cpp
+++ vizinhoProximo.cpp
@@ -23,17 +23,12 @@ void printSolucao(vector<int> &sol, int arraySize)
 
 vector<int> construtivo(){
     
-    // Custos de cada Nó para outro Nó
-    // Custo de ij = ji
     vector<vector<int>> costs = {
         {0,40,20,100},
         {40,0,50,10},
         {20,50,0,10},
         {100,10,10,0}
     };
-    
-    // Solucao inicial aleatória,
-    // Mesmo mudando os valores do nó, a solucao será a mesma
     vector<int> vertices = { 2, 3, 4};
     vector<int> sol = {1};
     int solCost = 0;
@@ -41,24 +36,22 @@ vector<int> construtivo(){
     int instanceSize = costs[1].size() - 1 ;
     int i,j;
 
+    int menor = 1000;
+    int menorJ = 1000;
     
-    while (vertices.size() != 0)
+    while (!vertices.empty())
     {
-        int menor = 1000;
-        int currentVertice = -1;
-
-        for(i = 0 ; i < vertices.size() ; i++){
-            if(costs[sol[sol.size()-1]-1][vertices[i]-1] < menor) {
-                menor = costs[sol[sol.size()-1]-1][vertices[i]-1];
-                currentVertice = i;
+        for(i=0;i<ROW;i++){
+            for(j=0;j<ROW;j++){
+                if(costs[i][j] < menor && costs[i][j] != 0){
+                    sol.push_back(j);
+                }
             }
+            int it = j - 1;
+            vertices.erase(vertices.begin() +  it);
         }
-
-
-        sol.push_back(vertices[currentVertice]);
-        vertices.erase(vertices.begin() + currentVertice);
     }
-    sol.push_back(1);
+    cout << solCost;
     return sol;
 }
 
